147d425421c7fbd2a7007bb7054c611b8d9fc7d4
Lander Andrade <lander.barros@gmail.com>
2026-05-18 18:24:59 -0300

    feat:  adicionar relacionamento ManyToOne entre Tarefa e Usuario.

--- src/main/java/com/forgile/taskforge/model/Tarefa.java
+++ src/main/java/com/forgile/taskforge/model/Tarefa.java
@@ -7,9 +7,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -43,4 +46,8 @@ public class Tarefa {
 
     @UpdateTimestamp
     private LocalDateTime dataAtualizacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 }
--- src/main/java/com/forgile/taskforge/repository/TarefaRepository.java
+++ src/main/java/com/forgile/taskforge/repository/TarefaRepository.java
@@ -1,8 +1,11 @@
 package com.forgile.taskforge.repository;
 
+import java.util.List;
+
 import com.forgile.taskforge.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
+    List<Tarefa> findByUsuarioEmail(String email);
 }
